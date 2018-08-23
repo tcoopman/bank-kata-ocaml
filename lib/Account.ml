68d9311cee05bd1c321e04c94f0e2204388a39a6
Thomas Coopman <thomas.coopman@gmail.com>
2018-08-23 22:35:15 +0200

    almost correct printing (now to the date)

--- lib/Account.ml
+++ lib/Account.ml
@@ -3,19 +3,24 @@ type transaction = {
   amount: float;
 }
 type account = transaction list
+
 let create () = []
 
-let deposit amount account = {date= ""; amount} :: account
-let withdrawal amount account = {date= ""; amount = amount *. -1.} :: account
+let deposit amount account = {date= "14/01/2012"; amount} :: account
+let withdrawal amount account = {date= "14/01/2012"; amount = amount *. -1.} :: account
 
 let print account =
+  let calculate account =
+    List.fold_right (fun transaction (total, x) -> (total +. transaction.amount, (total +. transaction.amount, transaction) :: x)) account (0., [])
+  in
   let buffer = Buffer.create (List.length account * 50) in
   let rec fill_buffer = function
     | [] -> ()
-    | transaction :: tl ->
-      Buffer.add_string buffer (Printf.sprintf "%s || %.2f\n" transaction.date transaction.amount);
+    | (total, transaction) :: tl ->
+      Buffer.add_string buffer (Printf.sprintf "%s || %.2f || %.2f\n" transaction.date transaction.amount total);
       fill_buffer tl
   in
+  let (_, calculated) = calculate account in
   Buffer.add_string buffer "date || amount || balance\n";
-  fill_buffer account;
+  fill_buffer calculated;
   print_string (Buffer.contents buffer)
--- test/AccountTest.ml
+++ test/AccountTest.ml
@@ -10,6 +10,6 @@ let%expect_test "Printing the statements should contain all transactions" =
   |> Lib.Account.print;
   [%expect{|
 date || amount || balance
-14/01/2012 || || 500.00 || 2500.00
+14/01/2012 || -500.00 || 2500.00
 13/01/2012 || 2000.00 || || 3000.00
 10/01/2012 || 1000.00 || || 1000.00 |}]
