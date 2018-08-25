87357bd6d2d9d8dfddd9bbd6f3aa784154b0648d
Thomas Coopman <thomas.coopman@gmail.com>
2018-08-25 11:43:16 +0200

    Introduce a Clock Module and pass it to the Account functor

--- lib/Account.ml
+++ lib/Account.ml
@@ -1,36 +1,46 @@
-type transaction = {
-  date: string;
-  amount: float;
-}
-type account = transaction list
+module Clock = struct
+  let now () = "SystemClock"
+end
 
-let create () = []
+module type Clock_S = module type of Clock
 
-let deposit ~amount ~on account = {date = on; amount} :: account
-let withdrawal ~amount ~on account = {date = on; amount = amount *. -1.} :: account
+module Make(Clock: Clock_S) = struct
+  type transaction = {
+    date: string;
+    amount: float;
+  }
+  type account = transaction list
 
-let print account =
-  let add_balance account =
-    let (_, with_calculated_balance) = List.fold_right (
-        fun transaction (total, calculated_transactions) -> 
-          (total +. transaction.amount, (total +. transaction.amount, transaction) :: calculated_transactions)
-      ) account (0., []) in
-    with_calculated_balance
-  in
-  let to_print_statements account_with_balance =
-    List.map (fun (total, transaction) -> 
-        Printf.sprintf "%s || %.2f || %.2f\n" transaction.date transaction.amount total
-      ) account_with_balance
-  in
-  let print_statements_to_string_with_header print_statements =
-    let buffer = Buffer.create (List.length account * 50) in
-    Buffer.add_string buffer "date || amount || balance\n";
-    List.iter (Buffer.add_string buffer) print_statements;
-    Buffer.contents buffer
-  in
+  let create () = []
 
-  account
-  |> add_balance
-  |> to_print_statements
-  |> print_statements_to_string_with_header
-  |> print_string
+  let deposit ~amount account = {date = Clock.now (); amount} :: account
+  let withdrawal ~amount account = {date = Clock.now (); amount = amount *. -1.} :: account
+
+  let print account =
+    let add_balance account =
+      let (_, with_calculated_balance) = List.fold_right (
+          fun transaction (total, calculated_transactions) -> 
+            (total +. transaction.amount, (total +. transaction.amount, transaction) :: calculated_transactions)
+        ) account (0., []) in
+      with_calculated_balance
+    in
+    let to_print_statements account_with_balance =
+      List.map (fun (total, transaction) -> 
+          Printf.sprintf "%s || %.2f || %.2f\n" transaction.date transaction.amount total
+        ) account_with_balance
+    in
+    let print_statements_to_string_with_header print_statements =
+      let buffer = Buffer.create (List.length account * 50) in
+      Buffer.add_string buffer "date || amount || balance\n";
+      List.iter (Buffer.add_string buffer) print_statements;
+      Buffer.contents buffer
+    in
+
+    account
+    |> add_balance
+    |> to_print_statements
+    |> print_statements_to_string_with_header
+    |> print_string
+end
+
+include Make(Clock)
--- test/AccountTest.ml
+++ test/AccountTest.ml
@@ -1,12 +1,25 @@
 open! Base
 open! Stdio
 
+module type TestDates = sig
+  val dates : string array
+end
+
+module TestClock (D : TestDates) = struct
+  let i = ref 0
+  let now () =
+    let date = D.dates.(!i) in
+    i := !i + 1;
+    date
+end
+
 let%expect_test "Printing the statements should contain all transactions" =
-  Lib.Account.create ()
-  |> Lib.Account.deposit ~amount:1000.0 ~on:"10/01/2012"
-  |> Lib.Account.deposit ~amount:2000.0 ~on:"13/01/2012"
-  |> Lib.Account.withdrawal ~amount:500.0 ~on:"14/01/2012"
-  |> Lib.Account.print;
+  let module Account = Lib.Account.Make(TestClock(struct let dates = [|"10/01/2012"; "13/01/2012"; "14/01/2012"|] end)) in
+  Account.create ()
+  |> Account.deposit ~amount:1000.0
+  |> Account.deposit ~amount:2000.0
+  |> Account.withdrawal ~amount:500.0
+  |> Account.print;
   [%expect{|
     date || amount || balance
     14/01/2012 || -500.00 || 2500.00
